02cb6344e583c951cb0e7553e99346e8ad034277
mrborghini <61702082+mrborghini@users.noreply.github.com>
2025-04-27 15:13:46 +0200

    feat: initialize project

---
+++ src/client/java/mrborghini/bettercords/BetterCordsClient.java
@@ -0,0 +1,10 @@
+package mrborghini.bettercords;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterCordsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
